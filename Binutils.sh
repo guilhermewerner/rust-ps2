2cb56cbddf13c82af27140b326343fc7ffc4e26e
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-09-15 14:29:23 -0300

    Create Binutils.sh

---
+++ Binutils.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -x
+
+main() {
+    mkdir -p /tmp/ps2/binutils /tmp/ps2/build
+
+    curl --retry 3 -sSfL "https://ftp.gnu.org/gnu/binutils/binutils-2.36.tar.gz" -O
+    tar -C /tmp/ps2/binutils --strip-components=1 -xf "binutils-2.36.tar.gz"
+
+    cd /tmp/ps2
+
+    cd build
+    ../binutils/configure --target=mips64el-none-elf
+    make "-j$(nproc)"
+    make install
+    cd ..
+
+    rm -rf /tmp/ps2
+}
+
+main "${@}"
